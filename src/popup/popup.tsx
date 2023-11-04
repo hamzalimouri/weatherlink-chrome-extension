4552cc494de9cc5522b3b644034b3ed53d51c1c8
Hamza Limouri <hamza@Hamzas-MacBook-Pro.local>
2023-11-04 20:56:40 +0100

    Added Input to add new cities.

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './popup.css'
 import { WeatherCard } from '../components/WeatherCard'
 import 'fontsource-roboto'
+import { Box, IconButton, InputBase, Paper, TextField } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>([
+    'London',
+    'New York',
+    'Tokyo',
+    'error',
+  ])
+
+  const [cityInput, setCityInput] = useState<string>('')
+
+  const handleCityButtonClick = () => {
+    if (cityInput === '') return
+    setCities([...cities, cityInput])
+    setCityInput('')
+  }
   return (
-    <div>
-      <WeatherCard city="Lodon" />
-	  <WeatherCard city="New York" />
-	  <WeatherCard city="Tokyo" />
-    </div>
+    <Box p={'4px'}>
+      <Paper
+        component='form'
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 300 }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder='New city'
+          value={cityInput}
+          onChange={(e) => setCityInput(e.target.value)}
+        />
+        <IconButton
+          type='button'
+          sx={{ p: '10px' }}
+          aria-label='add'
+          onClick={handleCityButtonClick}
+        >
+          <AddIcon />
+        </IconButton>
+      </Paper>
+
+      {cities.map((city, index) => (
+        <WeatherCard city={city} key={index} />
+      ))}
+    </Box>
   )
 }
 
